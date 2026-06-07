dbe5076b82e624d797291a4e148ec9a8c9d48964
anitharamesh-dataterrain <anitha@dataterrain.com>
2026-06-07 22:12:07 +0530

    GET api done

--- basic-core/backend/app.py
+++ basic-core/backend/app.py
@@ -11,6 +11,7 @@ conn = psycopg2.connect(
 )
 
 class APIHandler(BaseHTTPRequestHandler):
+
     def do_POST(self):
         if self.path == "/tasks":
             # get payload and header
@@ -27,7 +28,7 @@ class APIHandler(BaseHTTPRequestHandler):
 
             task_id = cur.fetchone()[0]
             conn.commit()
-            conn.close()
+            cur.close()
 
             # send response
             response = {
@@ -40,6 +41,29 @@ class APIHandler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(json.dumps(response).encode())
 
+    def do_GET(self):
+      if self.path == "/tasks":
+        cur = conn.cursor()
+        query = load_sql("get_tasks.sql")
+        cur.execute(query)  
+
+        rows = cur.fetchall()
+        cur.close()
+
+        tasks=[]
+
+        for row in rows:
+            tasks.append({
+                "id": row[0],
+                "title": row[1]
+            })
+        self.send_response(200)
+        self.send_header("Content-Type","application/json")
+        self.end_headers()
+        self.wfile.write(
+            json.dumps(tasks).encode()
+        )
+        
 server = HTTPServer(("localhost", 8000), APIHandler)
 
 print("Server started on port 8000")
